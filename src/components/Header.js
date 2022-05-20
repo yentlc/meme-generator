2f9032ab8888db4f15a9940bb8a794744e33d04f
yentlc <98667887+yentlc@users.noreply.github.com>
2022-05-20 12:41:49 -0300

    header component - making and styling similar to figma reference

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+export default function Header() {
+  return (
+    <header>
+      <div className='logo-section'>
+        <img
+          className='logo-troll'
+          src='./images/logo-troll.png'
+          alt='troll face logo'
+        />
+        <p className='logo-title'>Meme Generator</p>
+      </div>
+      <p className='text-course'>React Course - Project 3</p>
+    </header>
+  );
+}
