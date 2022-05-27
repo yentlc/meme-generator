d91d83151d743a89ff7e9712c3b2ff5e4268e876
yentlc <98667887+yentlc@users.noreply.github.com>
2022-05-27 15:37:36 -0300

    form updated

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,6 +2,14 @@ import { useState } from 'react';
 import DataMeme from '../DataMeme';
 
 export default function Form() {
+  //state meme object
+  const [meme, setMeme] = useState({
+    topText: '',
+    bottomText: '',
+    randomImage: 'https://i.imgflip.com/9ehk.jpg',
+  });
+
+  //MEME IMAGE
   function getImage() {
     let memeArr = DataMeme.data.memes;
     let url = memeArr[Math.floor(Math.random() * (memeArr.length - 1))].url;
@@ -10,20 +18,32 @@ export default function Form() {
       randomImage: url,
     }));
   }
-  const [meme, setMeme] = useState({
-    topText: '',
-    bottomText: '',
-    randomImage: 'https://i.imgflip.com/9ehk.jpg',
-  });
+
+  //MEME TEXT
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setMeme((prevText) => ({
+      ...prevText,
+      [name]: value,
+    }));
+  }
 
   return (
     <main>
       <form className='form'>
-        <input type='text' className='text-input-up' placeholder='Up text' />
+        <input
+          type='text'
+          className='text-input-up'
+          placeholder='Up text'
+          name='topText'
+          value={meme.topText}
+        />
         <input
           type='text'
           className='text-input-down'
           placeholder='Bottom text'
+          name='bottomText'
+          value={meme.bottomText}
         />
         <button type='button' onClick={getImage}>
           Get a new meme image <i className='fa-solid fa-image'></i>
